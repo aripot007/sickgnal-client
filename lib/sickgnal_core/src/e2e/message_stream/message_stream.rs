2e0553d98c2ec31e2a806d5b1431ddb3e233388d
Aristide Urli <aristide.urli@telecomnancy.net>
2026-03-03 21:40:13 -0500

    feat(e2e): Allow splitting message stream

--- lib/sickgnal_core/src/e2e/message_stream/message_stream.rs
+++ lib/sickgnal_core/src/e2e/message_stream/message_stream.rs
@@ -5,10 +5,22 @@ use crate::e2e::message::E2EMessage;
 
 /// Trait for sending and receiving E2E Messages
 #[async_trait]
-pub trait E2EMessageStream {
+pub trait E2EMessageStream: E2EMessageWriter + E2EMessageReader {
+    type Reader: E2EMessageReader + 'static;
+    type Writer: E2EMessageWriter + 'static;
+
+    /// Consumes the stream and return the two halves
+    fn split(self) -> (Self::Reader, Self::Writer);
+}
+
+#[async_trait]
+pub trait E2EMessageWriter: Send {
     /// Send an E2E message
     async fn send(&mut self, message: E2EMessage) -> Result<(), MessageStreamError>;
+}
 
+#[async_trait]
+pub trait E2EMessageReader: Send {
     /// Receive an E2E message
     async fn receive(&mut self) -> Result<E2EMessage, MessageStreamError>;
 }
